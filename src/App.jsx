c2c8da291b4900c0a97509dbe048539f99e34388
moosemorse <a.ivanov6174@gmail.com>
2026-06-01 19:41:42 +0100

    reset feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 import { useState } from 'react';
 
-export default function Board() { 
-  const [isX, setIsX] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function handleReset() {
+    setHistory([Array(9).fill(null)]);
+    setXIsNext(true);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <button type="button" className="reset-button" onClick={handleReset}>
+          Play Again
+        </button>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) { 
+  
   const winner = calculateWinner(squares);
-  let status = winner ? `Winner: ${winner}` : `Next player: ${isX ? 'X' : 'O'}`;
+  let status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) return;
+    if (squares[i] || winner) return;
     const newSquares = squares.slice();
-    newSquares[i] = isX ? 'X' : 'O';
-    setSquares(newSquares);
-    setIsX(!isX);
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    onPlay(newSquares);
   }
   
   return (     
